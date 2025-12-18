edcf7b648ebed595af6fe597c3ad88e15d979516
Rasheed Rahman <radiumrasheed@gmail.com>
2025-12-18 13:03:23 +0000

    feat: simulate rolling of a dice

--- index.js
+++ index.js
@@ -11,12 +11,32 @@ function rollDice() {
 
   for (let i = 0; i < numOfDice; i++) {
     const value = Math.floor(Math.random() * 6) + 1;
+    const angleInDegrees = (value * 50) + value
     values.push(value);
-    images.push(`<img src="./dice-img/dice${diceNames[value - 1]}.png" alt=" dice ${value} ">`);
+    images.push(`<img src="./dice-img/dice${diceNames[value - 1]}.png" alt=" dice ${value} " style="transform: rotate(${angleInDegrees}deg);" >`);
   }
 
   // display the results --diceResult--
   diceResult.textContent = `dice : ${values.join(", ")}`;
   // display the images --diceImages--
   diceImages.innerHTML = images.join("");
+}
+
+/**
+ * Calls a function x times with a given delay
+ *
+ * @param callback {function}
+ * @param times {number}
+ * @param delay {number}
+ */
+function repeatTask(callback = () => {}, times = 15, delay = 50) {
+  let count = 0;
+  const intervalId = setInterval(() => {
+    count++;
+    callback()
+
+    if (count >= times) {
+      clearInterval(intervalId);
+    }
+  }, delay--);
 }
